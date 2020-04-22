edde8ddea5a874e608f6c7bf512c0e1c19368281
leodinh <leo.atdinh@gmail.com>
2020-04-22 15:44:55 -0400

    optimized webpage, updated print cv feature, fixed minor errors

---
+++ README.md
@@ -0,0 +1,11 @@
+# Leo's Personal Portfolio
+
+My name is Leo. I am Software Developer.
+
+## My resume.
+
+[Click here to view my resume.](./Resume.pdf)
+
+## My Portfolio.
+
+[Click here to view my portfolio.](https://leodinh.github.io/)
--- js/script.js
+++ js/script.js
@@ -2,18 +2,17 @@
 // https://cssanimation.rocks/scroll-animations/
 var scroll = window.requestAnimationFrame ||
             function(callback){ window.setTimeout(callback, 1000/60)};
-var elementsToShow = document.querySelectorAll('.show-on-scroll');
 var nav = document.querySelector("nav");
 $('.navigation__item').on('click',()=>{
   $('#navi-toggle').prop("checked", false);
 })
 function loop() {
-
+    var elementsToShow = document.querySelectorAll('.show-on-scroll');
     elementsToShow.forEach(function (element) {
       if (isElementInViewport(element)) {
-        console.log(element);
         if(!element.classList.contains('is-visible')){
           element.classList.add('is-visible');
+          element.classList.remove('show-on-scroll');
         }
       }
     });
@@ -32,7 +31,6 @@ function isElementInViewport(el) {
     if (typeof jQuery === "function" && el instanceof jQuery) {
       el = el[0];
     }
-    console.log(el.getBoundingClientRect());
     var rect = el.getBoundingClientRect();
     return (
       (rect.top <= 0
@@ -49,7 +47,6 @@ function isElementInViewport(el) {
 //filter projects
 $('.project__filter').on( 'click', 'button', function() {
   const filterValue = $( this ).attr('data-filter');
-  console.log(filterValue);
   //$('.element-item').find('.element-item--inactive').removeClass('element-item--inactive');
   $('.element-item').each((i,element) => {
     let $elementItem = $(element);
@@ -82,40 +79,4 @@ $('.project__filter').each( function( i, buttonGroup ) {
     $( this ).addClass('button--active');
   });
 });
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 50)
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            //$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
 loop();  
